1762da47294d6290ff67b9e90e6148d14d42cf65
fafa123id <104996246+fafa123id@users.noreply.github.com>
2025-03-11 11:29:56 +0700

    Fix route for API

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
-
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return response()->json(['Service UBMagerAPI Sedang Berjalan, By UBMager'],201);
 });
 
+require __DIR__.'/auth.php';
