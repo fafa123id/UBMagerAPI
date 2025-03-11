f84bc3c0159467c14a833c82c22d38dc4c06d344
fafa123id <104996246+fafa123id@users.noreply.github.com>
2025-03-11 10:43:21 +0700

    Update AuthenticatedSessionController.php

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
@@ -13,26 +14,30 @@ class AuthenticatedSessionController extends Controller
     /**
      * Handle an incoming authentication request.
      */
-    public function store(LoginRequest $request): Response
+    public function store(LoginRequest $request): JsonResponse
     {
         $request->authenticate();
+        $user=$request->user();
+        $user->tokens()->delete();
 
-        $request->session()->regenerate();
+        $data =
+        [
+            'token'=>$user->createToken("thetoken". $user->email)->plainTextToken,
+            'message'=>'Login Berhasil'
+        ];
 
-        return response()->noContent();
+        return response()->json($data,201);
     }
 
     /**
      * Destroy an authenticated session.
      */
-    public function destroy(Request $request): Response
+    public function destroy(Request $request): JsonResponse
     {
-        Auth::guard('web')->logout();
+        $request->user()->currentAccessToken()->delete();
 
-        $request->session()->invalidate();
-
-        $request->session()->regenerateToken();
-
-        return response()->noContent();
+        return response()->json([
+            'message'=>'Berhasil logout'
+        ],201);
     }
 }
