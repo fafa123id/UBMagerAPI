2f0159c98fa8b25f024cd77d9be00b48a27ac880
fafa123id <104996246+fafa123id@users.noreply.github.com>
2025-03-17 16:04:29 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+Route::get('/api-routes', function () {
+    $routes = collect(Route::getRoutes())->map(function ($route) {
+        return [
+            'method' => implode('|', $route->methods()),
+            'uri' => $route->uri(),
+        ];
+    })->filter(function ($route) {
+        return str_starts_with($route['uri'], 'api/');
+    })->values();
+
+    return response()->json($routes);
+});
