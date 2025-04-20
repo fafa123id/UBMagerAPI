46d4a91f545a3626791782ef64144cacb3ce28d0
fafa123id <104996246+fafa123id@users.noreply.github.com>
2025-04-20 09:19:55 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,21 @@
 <?php
 
+use App\Http\Controllers\API\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['auth:sanctum'])->group(function(){
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    
+    //For Seller
+    Route::middleware(['Seller'])->group(function(){
+        Route::apiResource('product',ProductController::class)->except(['show','index']);
+    });
 });
+//guest
+Route::get('/product/{id}', [ProductController::class, 'show']);
+Route::get('/product', [ProductController::class, 'index']);
+    
 require __DIR__.'/auth.php';
