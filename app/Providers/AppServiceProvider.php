59e8f12d4c0c0259a9e11822a26f5fd2e4479a16
fafa123id <104996246+fafa123id@users.noreply.github.com>
2025-04-20 09:20:09 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,13 @@
 
 namespace App\Providers;
 
+use App\Observers\ProductObserver;
 use App\Services\UserRoleId;
 use App\Services\RouteInfo;
+use App\Models\Product;
+use App\Policies\ProductPolicy;
 use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,6 +24,10 @@ class AppServiceProvider extends ServiceProvider
         $this->app->singleton(RouteInfo::class,function($app){
             return new RouteInfo();
         });
+        $this->app->bind(
+            \App\Repositories\Abstract\ProductRepositoryInterface::class,
+            \App\Repositories\Concrete\ProductRepository::class
+        );
     }
 
     /**
@@ -27,8 +35,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Product::observe(ProductObserver::class);
         ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
             return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
         });
+        Gate::policy(Product::class, ProductPolicy::class);
     }
 }
