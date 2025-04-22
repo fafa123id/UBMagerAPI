6de12658baf1271d33a00998367cb39f0046b0a5
fafa123id <104996246+fafa123id@users.noreply.github.com>
2025-04-22 18:28:16 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,11 +2,14 @@
 
 namespace App\Providers;
 
+use App\Models\otp;
 use App\Observers\ProductObserver;
 use App\Services\UserRoleId;
 use App\Services\RouteInfo;
 use App\Models\Product;
+use App\Policies\OtpPolicy;
 use App\Policies\ProductPolicy;
+use App\Services\OtpMailer;
 use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
@@ -24,6 +27,9 @@ class AppServiceProvider extends ServiceProvider
         $this->app->singleton(RouteInfo::class,function($app){
             return new RouteInfo();
         });
+        $this->app->singleton(OtpMailer::class,function($app){
+            return new OtpMailer();
+        });
         $this->app->bind(
             \App\Repositories\Abstract\ProductRepositoryInterface::class,
             \App\Repositories\Concrete\ProductRepository::class
@@ -40,5 +46,6 @@ class AppServiceProvider extends ServiceProvider
             return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
         });
         Gate::policy(Product::class, ProductPolicy::class);
+        Gate::policy(otp::class, OtpPolicy::class);
     }
 }
