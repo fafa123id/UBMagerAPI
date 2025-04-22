f64a5995e29a2eb35232d89b2ae1a35ef198f00a
fafa123id <104996246+fafa123id@users.noreply.github.com>
2025-04-22 18:27:58 +0700

    Update services.php

--- config/services.php
+++ config/services.php
@@ -13,6 +13,9 @@ return [
     | a conventional file to locate the various service credentials.
     |
     */
+    'callmebot'=> [
+        'api_key' => env('CALLMEBOT_API_KEY'),
+    ],
 
     'postmark' => [
         'token' => env('POSTMARK_TOKEN'),
