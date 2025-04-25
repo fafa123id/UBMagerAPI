74479fd301e25c7875dc92ef1537bb652e1d70f1
fafa123id <104996246+fafa123id@users.noreply.github.com>
2025-04-25 13:09:44 +0700

    Make Transaction Model and Migration

---
+++ app/Models/Transaction.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'history_id',
+        'total_price',
+        'status',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
+    public function history()
+    {
+        return $this->belongsTo(History::class);
+    }
+}
