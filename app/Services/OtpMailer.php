f87ccb0e791c6117299e255844f2706cca811336
fafa123id <104996246+fafa123id@users.noreply.github.com>
2025-04-25 13:06:59 +0700

    Update OtpMailer.php

--- app/Services/OtpMailer.php
+++ app/Services/OtpMailer.php
@@ -9,8 +9,8 @@ class OtpMailer
 {
     protected $mailer;
 
-    public function sendOtp($email, $otp)
+    public function sendOtp($email, $otp, $for, $subject)
     {
-        return Mail::to($email)->send(new OtpMail($otp));
+        return Mail::to($email)->send(new OtpMail($otp, $for, $subject));
     }
 }
