db92cb25f01b040d732137d687414f6a5bb7b525
fafa123id <104996246+fafa123id@users.noreply.github.com>
2025-04-25 13:06:57 +0700

    Update ProductRepository.php

--- app/Repositories/Concrete/ProductRepository.php
+++ app/Repositories/Concrete/ProductRepository.php
@@ -1,37 +1,116 @@
 <?php
 namespace App\Repositories\Concrete;
 
+use App\Http\Resources\failReturn;
+use App\Http\Resources\ProductResource;
+use App\Http\Resources\successReturn;
 use App\Models\Product;
 use App\Repositories\Abstract\ProductRepositoryInterface;
 
 class ProductRepository implements ProductRepositoryInterface
 {
-    public function all()
+    public function all($type, $category , $query)
     {
-        return Product::with('user')->get();
+        $products = Product::query();
+        if ($type) {
+            $products->whereIn('type', $type);
+        }
+        if ($category) {
+            $products->whereIn('category', $category);
+        }
+        if ($query) {
+            $products->where(function ($q) use ($query) {
+                $q->where('name', 'like', '%' . $query . '%')
+                  ->orWhereHas('user', function ($userQuery) use ($query) {
+                      $userQuery->where('name', 'like', '%' . $query . '%');
+                  });
+            });
+        }
+        
+        return $products->get();
     }
-
+    public function getType()
+    {
+        return Product::distinct('type')->pluck('type');
+    }
+    public function getCategoryByType($type)
+    {
+        return Product::where('type', $type)->distinct('category')->pluck('category');
+    }
     public function find($id)
     {
-        return Product::with('user')->findOrFail($id);
+        $product = Product::find($id);
+        if (!$product) {
+            return new failReturn([
+                'message' => 'Product not found',
+                'status' => 404,
+            ]);
+        }
+        return new successReturn(
+            [
+                'status' => 200,
+                'message' => 'Product retrieved successfully',
+                'data' => new ProductResource($product),
+            ]
+        );
     }
 
     public function create(array $data)
     {
-        return Product::create($data);
+        $product = auth()->user()->product()->create($data);
+        return new successReturn(
+            [
+                'status' => 201,
+                'message' => 'Product created successfully',
+                'data' => new ProductResource($product),
+            ]
+        );
     }
 
     public function update($id, array $data)
     {
-        $product = Product::findOrFail($id);
+        $product = $this->tryCatch($id);
+        if (!$product) {
+            return new failReturn([
+                'message' => 'Product not found',
+                'status' => 404,
+            ]);
+        }
         $product->update($data);
-        return $product;
+        return new successReturn(
+            [
+                'status' => 200,
+                'message' => 'Product updated successfully',
+                'data' => new ProductResource($product),
+            ]
+        );
     }
 
     public function delete($id)
     {
-        $product = Product::findOrFail($id);
-        return $product->delete();
+        $product = $this->tryCatch($id);
+        if (!$product) {
+            return new failReturn([
+                'message' => 'Product not found',
+                'status' => 404,
+            ]);
+        }
+        $product->delete();
+        return new successReturn(
+            [
+                'status' => 204,
+                'message' => 'Product deleted successfully'
+            ]
+        );
+    }
+    private function tryCatch($id)
+    {
+        try {
+            $product = auth()->user()->product()->findOrFail($id);
+        } catch (\Exception $e) {
+            return null; 
+        }
+        return $product;
     }
 }
 
