d47de453b32615a8601d1da35096ee772f1015bb
fafa123id <104996246+fafa123id@users.noreply.github.com>
2025-04-25 13:06:16 +0700

    Update Product Model and Migration

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,12 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    protected $fillable = ['name', 'type', 'description','quantity', 'price', 'status','user_id'];
+    protected $fillable = ['name', 'type','category','quantity','price','description', 'status','user_id'];
     public function user()
     {
         return $this->belongsTo(User::class);
     }
-    
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
+    public function carts()
+    {
+        return $this->hasMany(Cart::class);
+    }
+
 }
 
 
---
+++ database/migrations/2025_04_23_130123_create_products_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name'); 
+            $table->string('type');
+            $table->string('category');
+            $table->text('description')->nullable();
+            $table->integer('quantity')->nullable(); 
+            $table->decimal('price', 10, 2); 
+            $table->enum('status', ['available', 'unavailable'])->default('available'); 
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->timestamps(); 
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
+
