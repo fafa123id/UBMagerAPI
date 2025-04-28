7ef68fda1cb48e0a105ad6caf7e0fccec91686be
fafa123id <104996246+fafa123id@users.noreply.github.com>
2025-04-28 14:54:36 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -13,12 +13,10 @@ Route::middleware(['auth:sanctum'])->group(function(){
     Route::get('/user', function (Request $request) {
         return request()->user();
     });
-    Route::post('/verify-email/send', [OtpSenderController::class, 'otpVerifySend'])->middleware(['throttle:2,1', 'unverified']);
-    Route::post('/verify-email', [VerifyEmailController::class, 'verifyEmail'])->middleware(['unverified']);
-    Route::post('/update-password', [ResetPasswordController::class, 'newPassword']);
     Route::apiResource('cart', CartController::class);
     Route::post('/cart/{id}/add-one', [CartController::class, 'addOne']);
     Route::post('/cart/{id}/remove-one', [CartController::class, 'removeOne']);
+    Route::delete('/cart', [CartController::class, 'destroyAll']);
     //For Seller
     Route::middleware(['Seller'])->group(function(){
         Route::apiResource('product',ProductController::class)->except(['show','index']);
@@ -29,9 +27,7 @@ Route::get('/product/{id}', [ProductController::class, 'show']);
 Route::get('/product', [ProductController::class, 'index']);
 Route::get('/product-type', [ProductController::class, 'getType']);
 Route::get('/product-category/{type}', [ProductController::class, 'getCategoryByType']);
-Route::post('/forgot-password', [OtpSenderController::class, 'otpResetSend'])->middleware(['throttle:2,1', 'verified']);
-Route::post('/forgot-password/verify', [ResetPasswordController::class, 'verifyOtp'])->middleware(['verified']);
-Route::post('/reset-password', [ResetPasswordController::class, 'resetPassword'])->middleware(['verified']);
+
 
 
 require __DIR__.'/auth.php';
