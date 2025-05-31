086d8b181a4eac64ccff0ec7eb503d032494f113
fafa123id <104996246+fafa123id@users.noreply.github.com>
2025-05-31 17:50:49 +0700

    removing cart routes

--- routes/Api/cart.php
+++
@@ -1,9 +0,0 @@
-<?php
-use App\Http\Controllers\Api\CartController;
-
-Route::middleware(['auth:sanctum'])->group(function(){
-    Route::apiResource('cart', CartController::class);
-    Route::post('/cart/{id}/add-one', [CartController::class, 'addOne']);
-    Route::post('/cart/{id}/remove-one', [CartController::class, 'removeOne']);
-    Route::delete('/cart', [CartController::class, 'destroyAll']);
-});
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-require __DIR__.'/Api/cart.php';
 require __DIR__.'/Api/product.php';
 require __DIR__.'/Api/user.php';
 require __DIR__.'/auth.php';
