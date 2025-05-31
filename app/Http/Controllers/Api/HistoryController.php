c405750ccb70fce65473b301e1fa57dca7926e26
fafa123id <104996246+fafa123id@users.noreply.github.com>
2025-05-31 17:48:13 +0700

    Make History Controller

---
+++ app/Http/Controllers/Api/HistoryController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Resources\successReturn;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Transaction;
+
+class HistoryController extends Controller
+{
+}
