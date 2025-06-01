f1ac7d6921565c730882014844fc5c95b35547b9
fafa123id <104996246+fafa123id@users.noreply.github.com>
2025-06-01 08:46:41 +0700

    fix title view finish redirect

--- resources/views/midtrans/finish.blade.php
+++ resources/views/midtrans/finish.blade.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-    <title>Payment Completed</title>
+    <title>Action Finished</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script>
         window.onload = function () {
