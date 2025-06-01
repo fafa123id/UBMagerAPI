90d25da3a6952d7575daba105492fbdf0eb006fb
fafa123id <104996246+fafa123id@users.noreply.github.com>
2025-06-02 06:08:05 +0700

    fix logo scribe

--- config/scribe.php
+++ config/scribe.php
@@ -180,7 +180,7 @@ return [
     // For example, if your logo is in public/img:
     // - 'logo' => '../img/logo.png' // for `static` type (output folder is public/docs)
     // - 'logo' => 'img/logo.png' // for `laravel` type
-    'logo' => '/favicon.ico',
+    'logo' => '/favicon.png',
 
     // Customize the "Last updated" value displayed in the docs by specifying tokens and formats.
     // Examples:
