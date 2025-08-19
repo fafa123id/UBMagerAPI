dabc5284c30c28a7316036f2dbb2796037aee67a
fafa123id <104996246+fafa123id@users.noreply.github.com>
2025-08-20 06:29:44 +0700

    add cdn tailwind

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Portal UBMager API</title>
+    <script src="https://cdn.tailwindcss.com"></script>
     <style>
         body {
             background-color: #000;
