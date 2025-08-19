8b0a02cae0f44d23f74323fe4e8176c3901c132b
fafa123id <104996246+fafa123id@users.noreply.github.com>
2025-08-19 10:11:41 +0700

    add trust proxies middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -23,6 +23,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->validateCsrfTokens(except: [
             '*',
         ]);
+        $middleware->trustProxies("*");
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
