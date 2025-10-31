0189022d54e6e3da3849d41be631cec04dba50ce
fafa123id <ahmad.fauzan.rozikin@gmail.com>
2025-11-01 03:40:33 +0700

    refactor: simplify authentication logic in AuthenticatedSessionController

--- Jenkinsfile
+++ Jenkinsfile
@@ -22,7 +22,7 @@ pipeline {
         stage('Build and Deploy Application') {
             steps {
                 echo 'Menghapus Image Lama'
-                sh 'docker compose down -v --remove-orphans'
+                sh 'docker compose down --remove-orphans'
                 echo '--- MEMBANGUN IMAGE APLIKASI BARU ---'
                 sh 'docker compose build'
 
--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -28,31 +28,13 @@ class AuthenticatedSessionController extends Controller
             'password' => 'required|string',
         ]);
 
-        // 1. Cek kredensial manual
-        $user = User::where('email', $credentials['email'])->first();
-
-        if (!$user || !Hash::check($credentials['password'], $user->password)) {
+        if (!auth()->attempt($credentials)) {
             return response()->json(['message' => 'Email atau password salah.'], 401);
         }
-
-        // 2. Kredensial valid, minta token ke Passport
-        // Kita "menembak" request ke endpoint /oauth/token internal
-        $response = Http::asForm()->post(url('/oauth/token'), [
-            'grant_type' => 'password',
-            'client_id' => env('PASSPORT_PASSWORD_GRANT_CLIENT_ID'),
-            'client_secret' => env('PASSPORT_PASSWORD_GRANT_CLIENT_SECRET'),
-            'username' => $request->email,
-            'password' => $request->password,
-            'scope' => '*',
-        ]);
-
-        // 3. Cek jika request token gagal
-        if ($response->failed()) {
-            return response()->json(['message' => 'Gagal mendapatkan token.'], $response->status());
-        }
-
+        $user = auth()->user();
+        $token = $user->createToken('UserToken')->accessToken;
         // 4. Kirim kembali respons token (berisi access_token dan refresh_token)
-        return response()->json($response->json());
+        return response()->json(['access_token' => $token]);
     }
 
 
