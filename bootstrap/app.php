4dc99b8c8204399708bada85fb70a52a9fe38478
fafa123id <ahmad.fauzan.rozikin@gmail.com>
2025-10-31 16:53:25 +0700

    refactor: reorganize middleware definitions and improve formatting in app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,26 +1,40 @@
 <?php
 
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Http\Middleware\HandleCors;
+use Illuminate\Routing\Middleware\SubstituteBindings;
+use Illuminate\Session\Middleware\StartSession;
+use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->api(prepend: [
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+        $middleware->group('api', [
+            EnsureFrontendRequestsAreStateful::class, // detect SPA origins
+            EncryptCookies::class,
+            AddQueuedCookiesToResponse::class,
+            StartSession::class,
+            ShareErrorsFromSession::class,
+            VerifyCsrfToken::class,                  // CSRF untuk cookie-based
+            SubstituteBindings::class,
+            'throttle:api',
         ]);
         $middleware->prepend(HandleCors::class);
         $middleware->alias([
             'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
             'unverified' => \App\Http\Middleware\EnsureUnverifiedUser::class,
-            'Seller'=>\App\Http\Middleware\Seller::class
+            'Seller' => \App\Http\Middleware\Seller::class
         ]);
         $middleware->validateCsrfTokens(except: [
             '*',
