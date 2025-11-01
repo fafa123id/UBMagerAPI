264dfa8772ebd40c96935ce3ee03939927800ded
fafa123id <ahmad.fauzan.rozikin@gmail.com>
2025-11-01 17:45:25 +0700

    refactor: update permissions for oauth-private.key to enhance security

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,6 +15,7 @@ php artisan l5-swagger:generate
 
 chown -R www-data:www-data storage bootstrap/cache
 chmod -R 775 storage bootstrap/cache
+chmod -R 660 storage/oauth-private.key
 
 composer dump-autoload -o
 
