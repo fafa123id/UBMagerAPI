bd294d35224fddd7ad5358ee144b98ec2126cac7
fafa123id <ahmad.fauzan.rozikin@gmail.com>
2025-11-04 18:58:33 +0700

    fix: refine email verification condition in update method of UserRepository

--- app/Repositories/Concrete/UserRepository.php
+++ app/Repositories/Concrete/UserRepository.php
@@ -20,7 +20,7 @@ class UserRepository implements UserRepositoryInterface
     public function update($id, array $data, string $oldEmail)
     {
         $user = User::findOrFail($id);
-        if (($data["email"] !== null) && ($data["email"] !== $oldEmail)) {
+        if ((isset($data["email"]) && ($data["email"] !== $oldEmail))) {
             $data['status'] = 'unverified';
             $data['email_verified_at'] = null;
         }
