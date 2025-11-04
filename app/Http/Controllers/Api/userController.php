c209c57b69aa64e6c416c6b63b7dfff704c36a9b
fafa123id <ahmad.fauzan.rozikin@gmail.com>
2025-11-04 10:27:51 +0700

    refactor: remove debug statement from user profile update method

--- app/Http/Controllers/Api/userController.php
+++ app/Http/Controllers/Api/userController.php
@@ -67,7 +67,6 @@ class userController extends Controller
             $imagePath = config('filesystems.disks.s3.url') . $request->file('image')->store('images', 's3');
             $request->merge(['image' => $imagePath]);
         }
-        dd( $request->all());
         return $this->users->update($id, $request->all());
     }
     /**
