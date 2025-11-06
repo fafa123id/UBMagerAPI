0391d7eb5df4eeec0963364a5e74beb68818fa67
fafa123id <ahmad.fauzan.rozikin@gmail.com>
2025-11-06 18:16:41 +0700

    fix: refactor user creation logic in Google authentication callback

--- app/Http/Controllers/Auth/GoogleAuthController.php
+++ app/Http/Controllers/Auth/GoogleAuthController.php
@@ -19,24 +19,33 @@ class GoogleAuthController extends Controller
     public function callback()
     {
         $googleUser = Socialite::driver('google')->stateless()->user();
+
+        $user = User::where('gmail', $googleUser->email)->first();
         $password = Hash::make(Str::random(24));
-        $user = User::updateOrCreate(
-            [
+        if (!$user) {
+            $user = User::create(
+
+                [
+                    'name' => $googleUser->name,
+                    'username' => preg_replace('/\s+/', '', strtolower($googleUser->name)) . rand(1000, 9999),
+                    'gmail' => $googleUser->email,
+                    'password' => $password,
+                    'google_id' => $googleUser->id,
+                ]
+            );
+        } else {
+            $user->update([
+                'password' => $password,
                 'google_id' => $googleUser->id,
-            ],
-            [
-                'name' => $googleUser->name,
-                'username' => preg_replace('/\s+/', '', strtolower($googleUser->name)) . rand(1000, 9999),
-                'email' => $googleUser->email,
-                'password' => $password
-            ]
-        );
-         try {
+            ]);
+        }
+
+        try {
             $tokenResp = Http::asForm()->post(url('/oauth/token'), [
                 'grant_type' => 'password',
                 'client_id' => config('services.passport.password_client_id'),
                 'client_secret' => config('services.passport.password_client_secret'),
-                'username' => $googleUser->email,
+                'username' => $user->gmail,
                 'password' => $password,
                 'scope' => '',
             ]);
@@ -50,7 +59,7 @@ class GoogleAuthController extends Controller
 
             $accessToken = $tokenResp->json()['access_token'];
             $refreshToken = $tokenResp->json()['refresh_token'];
-            return redirect(env('FRONTEND_URL') . '/auth/callback?token=' . $accessToken .'&refresh='. $refreshToken);
+            return redirect(env('FRONTEND_URL') . '/auth/callback?token=' . $accessToken . '&refresh=' . $refreshToken);
         } catch (\Throwable $e) {
             return response()->json([
                 'message' => 'Terjadi kesalahan saat meminta token.',
