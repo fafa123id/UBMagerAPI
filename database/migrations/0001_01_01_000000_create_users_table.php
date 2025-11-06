0b3b18caced8fcc5885281d7fb6cc0ed079a8156
fafa123id <ahmad.fauzan.rozikin@gmail.com>
2025-11-06 18:52:31 +0700

    fix: make password field nullable in users table migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('bio')->nullable();
             $table->string('phone')->nullable()->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
             $table->tinyInteger('role_id')->default(0);
             $table->enum('status', ['verified', 'unverified'])->default('unverified');
             $table->text('address')->nullable();
