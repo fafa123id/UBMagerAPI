8b1fc7bfe7ad1e8dc35dc7d90f16ba1501380783
fafa123id <ahmad.fauzan.rozikin@gmail.com>
2025-11-06 13:34:21 +0700

    fix: update email retrieval in login to use get() method for consistency

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -28,7 +28,7 @@ class AuthenticatedSessionController extends Controller
             'emailor_username' => 'required|string',
             'password' => 'required|string',
         ]);
-        $email = User::where('email', $request->emailor_username)->orWhere('username', $request->emailor_username)->firstOrFail()->email;
+        $email = User::where('email', $request->emailor_username)->orWhere('username', $request->emailor_username)->get('email')->firstOrFail()->email;
         $credentials = [
             'email' => $email,
             'password' => $request->password,
