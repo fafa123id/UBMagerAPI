74ee33331f728c93bb4036f1ab526181de1ce00b
fafa123id <ahmad.fauzan.rozikin@gmail.com>
2025-11-06 18:16:36 +0700

    fix: update login method to include users with null google_id

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -28,7 +28,7 @@ class AuthenticatedSessionController extends Controller
             'emailor_username' => 'required|string',
             'password' => 'required|string',
         ]);
-        $email = User::where('email', $request->emailor_username)->orWhere('username', $request->emailor_username)->get('email')->firstOrFail()->email;
+        $email = User::where('email', $request->emailor_username)->orWhere('username', $request->emailor_username)->orWhere('google_id', null)->firstOrFail()->email;
         $credentials = [
             'email' => $email,
             'password' => $request->password,
