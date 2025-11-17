4d018535d2c2d88f3255867a54947e09a14fa297
fafa123id <ahmad.fauzan.rozikin@gmail.com>
2025-11-18 02:11:51 +0700

    fix: add gmail field to user resource transformation

--- app/Http/Resources/userResource.php
+++ app/Http/Resources/userResource.php
@@ -22,6 +22,7 @@ class userResource extends JsonResource
             'email' => $this->email,
             'phone' => $this->phone,
             'address' => $this->address ?? null,
+            'gmail' => $this->gmail,
             'image' => $this->image ? config('filesystems.disks.s3.url') . $this->image : 'https://static.vecteezy.com/system/resources/thumbnails/020/765/399/small/default-profile-account-unknown-icon-black-silhouette-free-vector.jpg',
             'role_id' => $this->role_id,
             'is_verified' => $this->email_verified_at ? true : false,
