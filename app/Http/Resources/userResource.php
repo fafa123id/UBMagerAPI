9743f713deab50a9c79203cf65926f4dbedcaadf
fafa123id <ahmad.fauzan.rozikin@gmail.com>
2025-11-17 09:00:55 +0700

    fix: add phone_is_set field to user resource transformation

--- app/Http/Resources/userResource.php
+++ app/Http/Resources/userResource.php
@@ -26,6 +26,7 @@ class userResource extends JsonResource
             'role_id' => $this->role_id,
             'is_verified' => $this->email_verified_at ? true : false,
             'password_is_set' => $this->password ? true : false,
+            'phone_is_set' => $this->phone ? true : false,  
             'created_at' => $this->created_at->toDateTimeString(),
             'updated_at' => $this->updated_at->toDateTimeString(),
             'rating' => $this->role_id == 1 ? (float) $this->getRatingFromAllProduct() : null,
