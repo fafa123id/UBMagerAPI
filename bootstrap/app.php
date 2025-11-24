9671979c0edac79880194dda459c0dd9d3ef238f
fafa123id <ahmad.fauzan.rozikin@gmail.com>
2025-11-25 04:35:50 +0700

    fix: append HandleCors middleware to the middleware stack for proper CORS handling

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,6 +21,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->statefulApi();
+        $middleware->append(HandleCors::class);
         $middleware->alias([
             'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
             'unverified' => \App\Http\Middleware\EnsureUnverifiedUser::class,
