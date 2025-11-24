e9ab6e2acf4b2ccb8b68078f8034fb213cf743a2
fafa123id <ahmad.fauzan.rozikin@gmail.com>
2025-11-25 06:16:14 +0700

    fix: remove stateful API middleware from application configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -20,7 +20,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->statefulApi();
         $middleware->alias([
             'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
             'unverified' => \App\Http\Middleware\EnsureUnverifiedUser::class,
