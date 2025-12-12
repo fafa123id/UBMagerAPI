46805c0fb0d1c55c8822cf1b3bf32b823a45832b
fafa123id <ahmad.fauzan.rozikin@gmail.com>
2025-12-13 04:26:47 +0700

    feat: add rating count and page count methods to RatingController with corresponding routes

--- app/Http/Controllers/Api/RatingController.php
+++ app/Http/Controllers/Api/RatingController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Rating;
 
 class RatingController extends Controller
 {
@@ -58,5 +59,71 @@ class RatingController extends Controller
             'success' => true,
             'data' => $rating,
         ], 201);
+    }
+    private function buildQuery($rating, $id)
+    {
+        $ratings = Rating::where('product_id', $id)::with(['user:id,name,image'])
+            ->where('rating', '<=', $rating)
+            ->orderBy('created_at', 'desc')->orderBy('rating', 'desc');
+        return $ratings;
+    }
+    /**
+     * GET: /api/rating/count/{id}
+     * 
+     * Count ratings for a specific product with optional filtering by rating value.
+     * This method allows users to get the count of ratings for a product, filtered by a maximum rating value.
+     * @authenticated
+     */
+    public function count(Request $request, $id)
+    {
+        $ratings = $this->buildQuery(5, $id);
+        return response()->json([
+            'success' => true,
+            'data' => [
+                'count' => $ratings->count(),
+            ],
+        ], 200);
+    }
+    /**
+     * GET: /api/rating/page-count/{id}
+     * 
+     * Retrieve the page count of ratings for a specific product with optional filtering by rating value.
+     * This method allows users to get the total number of pages of ratings for a product, filtered by a maximum rating value, based on a specified number of ratings per page.
+     * @authenticated
+     */
+    public function pageCount(Request $request, $id){
+        $rating = $request->query('rating', 5);
+        $perpage = $request->query('perpage', 5);
+        $ratings = $this->buildQuery($rating, $id);
+        $pageCount = ceil($ratings->count() / $perpage);
+        return response()->json([
+            'success' => true,
+            'data' => [
+                'page_count' => $pageCount,
+            ],
+        ], 200);
+    }
+     /**
+     * GET: /api/rating/{id}
+     * 
+     * Retrieve ratings for a specific product with optional filtering by rating value.
+     * This method allows users to fetch ratings for a product, filtered by a maximum rating value, and supports pagination.
+     * @authenticated
+     */
+    public function get(Request $request, $id)
+    {
+        $rating = $request->query('rating', 5);
+        $perpage = $request->query('perpage', 5);
+        $page = $request->query('page', 1);
+        $ratings = $this->buildQuery($rating, $id);
+        if ($page && $perpage) {
+            $ratings->skip(($page - 1) * $perpage)->take($perpage);
+        }else{
+            $ratings->get();
+        }
+        return response()->json([
+            'success' => true,
+            'data' => $ratings,
+        ], 200);
     }
 }
--- routes/Api/rating.php
+++ routes/Api/rating.php
@@ -5,3 +5,6 @@ use App\Models\Rating;
 use Illuminate\Support\Facades\Route;
 Route::post('/rating/{id}', [RatingController::class ,'store'])
     ->middleware('auth:api');
+Route::get('/rating/{id}', [RatingController::class ,'get']);
+Route::get('/rating/page-count/{id}', [RatingController::class ,'pageCount']);
+Route::get('/rating/count/{id}', [RatingController::class ,'count']);
