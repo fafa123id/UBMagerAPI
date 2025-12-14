3fcf11e47d9f74c4c344866d3e7bf3f5b2908399
fafa123id <ahmad.fauzan.rozikin@gmail.com>
2025-12-14 13:32:13 +0700

    feat: update payment callback URL to use dynamic frontend URL with order ID

--- app/Http/Controllers/Api/CheckoutController.php
+++ app/Http/Controllers/Api/CheckoutController.php
@@ -158,7 +158,7 @@ class CheckoutController extends Controller
                 'item_details' => $itemDetails,
                 'customer_details' => $customerDetails,
                 'callbacks' => [
-                    'finish' => url('/api/payment/finish'),
+                    'finish' => env('FRONTEND_URL') . `/order/`. $order->id,
                 ]
             ];
             if ($enabledPayments) {
