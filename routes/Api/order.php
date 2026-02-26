0670dad57a2c1558a38485321459baf1dc56c590
fafa123id <ahmad.fauzan.rozikin@gmail.com>
2026-02-26 18:17:10 +0700

    fix: correct endpoint path for downloading receipts

--- routes/Api/order.php
+++ routes/Api/order.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:api'])->group(function () {
     // Order endpoints
-    Route::get('/order/{id}/receipt', [OrderController::class,'downloadReceipt']);
+    Route::get('/orders/{id}/receipt', [OrderController::class,'downloadReceipt']);
     Route::get('/orders/{id}/finish', [OrderController::class, 'finishOrder']);
     Route::get('/orders', [OrderController::class, 'index'])->middleware('Seller');
     Route::get('/orders/{id}/process', [OrderController::class, 'proccessOrder'])->middleware('Seller');
